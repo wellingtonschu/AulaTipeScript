b3d126c490d67741422a5b07d6b994d4a4222496
Wellington Schütz <wellington.schu@live.com>
2016-10-07 13:04:57 -0300

    Aula 01 e Aula 02(Parcial)

---
+++ MVA/Cliente.ts
@@ -0,0 +1,8 @@
+import Pessoa = require('./Pessoa');
+
+class Cliente extends Pessoa {
+    public codigoCliente: number;
+}
+
+Object.seal(Cliente);
+export = Cliente;
---
+++ MVA/Funcionario.ts
@@ -0,0 +1,9 @@
+import Pessoa = require('./Pessoa');
+
+class Funcionario extends Pessoa {
+    public salario: number;
+    public codigoFuncionario: number;        
+}
+
+Object.seal(Funcionario);
+export = Funcionario;
